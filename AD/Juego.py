52e27468fb2d5187bf06b3849338d6a23647b86c
PabloDLVR <pablovararodriguez@gmail.com>
2025-09-30 10:44:46 +0200

    Update

--- AD/Juego.py
+++ AD/Juego.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-tiempo_limite = random.randint(8, 15)
+tiempo_limite = random.randint(8, 15)   
 numero_secreto = random.randint(1, 100)
 
 print(f"Tienes {tiempo_limite} segundos para adivinar el número entre 1 y 100.")
@@ -10,19 +10,19 @@ inicio = time.time()
 
 while True:
     if time.time() - inicio > tiempo_limite:
-        print(f"⏰ Se acabó el tiempo. El número era {numero_secreto}.")
+        print(f"Se acabó el tiempo. El número era {numero_secreto}.")
         break
 
     intento_str = input("Adivina el número: ")
 
     if not intento_str.isdigit():
-        print("⚠️ Solo se permiten números enteros.")
+        print("Solo se permiten números enteros.")
         continue
 
     intento = int(intento_str)
 
     if intento == numero_secreto:
-        print("🎉 ¡Felicidades! Has adivinado el número.")
+        print("Has adivinado el número.")
         break
     elif intento < numero_secreto:
         print("El número es mayor.")
