096eab36cb613c6de14aaa549a5e9f132036ba47
PabloDLVR <pablovararodriguez@gmail.com>
2025-10-02 14:19:24 +0200

    Update

--- PMDM/untitled/src/model/Jefes.kt
+++ PMDM/untitled/src/model/Jefes.kt
@@ -2,4 +2,17 @@ package model
 
 class Jefes(nombre: String, apellido: String, dni: String, salario: Double, var nivelResponsablidad: Int) :
     Trabajador(nombre, apellido, dni, salario) {
+    override fun calcularSalarioNeto(): Double {
+        nivelResponsablidad = (1..10).random()
+        if (nivelResponsablidad >= 3) {
+            salario + (salario * 1.3)
+        } else if (nivelResponsablidad < 3){
+             salario - (salario * 1.3)
+        }
+        return salario
+    }
+
+    override fun mostrarDatos() {
+        super.mostrarDatos()
+    }
 }
