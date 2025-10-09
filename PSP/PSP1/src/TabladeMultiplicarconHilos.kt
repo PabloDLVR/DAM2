ebbbc1d407811895fcaaefe2a70dceb85b2a63e4
PabloDLVR <pablovararodriguez@gmail.com>
2025-10-09 14:29:41 +0200

    Update

--- PSP/PSP1/src/TabladeMultiplicarconHilos.kt
+++ PSP/PSP1/src/TabladeMultiplicarconHilos.kt
@@ -1,18 +1,16 @@
-//Tabla de multiplicar del 1 al 10 con hilos en orden
 fun main() {
     val hilos = mutableListOf<Thread>()
     for (i in 1..10) {
         val hilo = Thread {
             for (j in 1..10) {
                 println("$i x $j = ${i * j}")
-                Thread.sleep(100) // Simula un pequeño retraso
+                Thread.sleep(100)
             }
         }
         hilos.add(hilo)
     }
     for (hilo in hilos) {
         hilo.start()
-        hilo.join() // Espera a que el hilo termine antes de iniciar el siguiente
+        hilo.join()
     }
 }
-
