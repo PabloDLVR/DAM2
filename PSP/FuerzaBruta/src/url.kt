28d7e62bb1c3a55ba151fed5d285a6929ce13ed9
PabloDLVR <pablovararodriguez@gmail.com>
2025-10-30 13:00:10 +0100

    Update

---
+++ PSP/FuerzaBruta/src/url.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URI
+
+
+fun main() {
+    val url = URI("http://192.168.2.216/prueba.html").toURL()
+
+    val conexion = url.openConnection()
+    //conexion.setRequestProperty("User-Agent", "CESJP")
+    val input = conexion.getInputStream()
+
+    val buffer = BufferedReader(InputStreamReader(input))
+
+    var linea: String?
+
+    while (buffer.readLine().also { linea = it } != null) {
+        println(linea)
+    }
+}   
