7344ea29b1ea75d64d7f4989fbb07035b2283eaa
PabloDLVR <pablovararodriguez@gmail.com>
2025-11-18 18:39:25 +0100

    Update

---
+++ DI/Examen_JAVAFX/src/main/java/com/example/examen_javafx/database/DBConnection.java
@@ -0,0 +1,32 @@
+package com.example.examen_javafx.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnection {
+
+    // conexion a la base de datos
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            createConnection();
+        }
+        return connection;
+    }
+
+    private static void createConnection() {
+        String user = "root";
+        String pass = "";
+        try {
+            connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:%s/%s",
+                    SchemaDB.URL, SchemaDB.PORT, SchemaDB.DB_NAME), user, pass);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+    }
+
+
+}
---
+++ DI/Examen_JAVAFX/src/main/java/com/example/examen_javafx/database/SchemaDB.java
@@ -0,0 +1,16 @@
+package com.example.examen_javafx.database;
+
+public interface SchemaDB {
+
+    String URL = "127.0.0.1";
+    String PORT = "3306";
+    String DB_NAME = "";
+    String TABLE_ = "";
+    String COL_ = "";
+    String COL_ = "";
+    String COL_ = "";
+    String COL_ = "";
+    String COL_ = "";
+    String COL_ = "";
+
+}
