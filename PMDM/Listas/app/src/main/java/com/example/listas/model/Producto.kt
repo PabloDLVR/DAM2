4eeba7c438bb4520e0c3acb9a3c9aaaf3e6661d6
PabloDLVR <pablovararodriguez@gmail.com>
2025-11-25 10:05:04 +0100

    Update

---
+++ PMDM/Listas/app/src/main/java/com/example/listas/model/Producto.kt
@@ -0,0 +1,13 @@
+package com.example.listas.model
+
+class Producto(
+    var id: Int,
+    var nombre: String,
+    var stock: Int,
+    var precio: Double,
+    var descripcion: String,
+    var categoria: String,
+    var imagen: String
+) {
+
+}
